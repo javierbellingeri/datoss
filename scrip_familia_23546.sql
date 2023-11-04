a9bd7870ea6cc9b999618e0186892cccdc0bb515
javierbellingeri <142115524+javierbellingeri@users.noreply.github.com>
2023-11-04 01:14:44 -0300

    Add files via upload

---
+++ scrip_familia_23546.sql
@@ -0,0 +1,7 @@
+SELECT * FROM com_23546.familia;
+INSERT INTO familia(nombre, apellido, parentesco, edad, localidad)
+VALUES ("javier", "bellingeri", "padre", 44, "gualeguay"),
+("gimena", "arribillaga", "madre", 51, "gualeguay"),
+("juliana", "bellingeri", "hija", 30, "gualeguay"),
+("mariano", "bellingeri", "hijo", 26, "gualeguay"),
+("josefina", "bellingeri", "hija", 21, "gualeguay");
